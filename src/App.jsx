834574f939193e6116b3c2d207ee9552475f43f1
JuanVal092002 <juancarlospastasvalencia@hotmail.com>
2024-06-18 18:20:04 -0500

    primer cambio

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import iniciosesion from './src/pages/inicio_sesion/inicio_sesion'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <h1>hola Urban Point</h1>
+      <iniciosesion/>
     </>
   )
 }
