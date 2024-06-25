3cae41523aa557c54325c324a0b01fa12eddbd4f
cristian ipia <162381718+MiguelitoFREEFAY@users.noreply.github.com>
2024-06-25 14:16:11 -0500

    primer comit

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,16 @@
-import { BrowserRouter as Router, Route, Routes, Form } from "react-router-dom";
-import BarraLateral from "./components/BarraLateral";
-import VerificacionCuenta from "./pages/VerificacionCuenta";
-import PerfilAdmins from "./pages/PerfilAdmins";
-import Contratos from "./pages/Contratos";
-import Publicaciones from "./pages/Publicaciones";
-import Incidentes from "./pages/Incidentes";
+import { useState } from 'react'
+import './App.css'
+import Clock from "./components/Clock"
 
-
-const App = () => {
-    return(
-        <Router>
-            <div className="flex">
-            <BarraLateral />
-            <div className="ml-64 p-4 w-full">
-            <Routes>
-                <Route path="/" element={<PerfilAdmins />}/>
-                <Route path="/verificacionCuenta" element={<VerificacionCuenta />}/>
-                <Route path="/Contratos" element={<Contratos />}/>
-                <Route path="/Publicaciones" element={<Publicaciones />}/>
-                <Route path="/Incidentes" element={<Incidentes />}/>
-            </Routes>
-            </div>
-            </div>
-        </Router>
-    )
+function App() {
+  const [count, setCount] = useState(0)
+    return (
+      <div>
+        <main>
+          <Clock />
+        </main>
+      </div>
+    );
 }
 
-export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default App
