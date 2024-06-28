51640656012a915ed3ddfeeb5151dd0883b18715
ChexTP <chechexteroth@gmail.com>
2024-06-28 15:37:52 -0500

    landing al 60%

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import SearchBar from './components/Barra_busqueda/busqueda';
 import InicioSesion from './pages/inicio_sesion/iniciosesion'; // Ajusta la ruta de importación según la estructura de tu proyecto
 import Registro from './pages/inicio_sesion/registrarse'; // Ajusta la ruta de importación según la estructura de tu proyecto
 import Landing from './components/LADING/lading';
+import Menu from './components/filtros/menu';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Header />
         <SearchBar />
         <Landing/>
+        <Menu/>
         <Routes>
           <Route path="/iniciosesion" element={<InicioSesion />} />
           <Route path="/registro" element={<Registro />} />
