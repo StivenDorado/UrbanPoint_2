c9e4e43b4a3d39c274ba477a34fd4d0faab0acb5
Stiven Dorado <golds9013@gmail.com>
2024-07-02 17:23:21 -0500

    ultimo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/header/headerlg';
-import SearchBar from './components/Barra_busqueda/busqueda';
-import InicioSesion from './pages/inicio_sesion/iniciosesion'; // Ajusta la ruta de importación según la estructura de tu proyecto
-import Registro from './pages/inicio_sesion/registrarse'; // Ajusta la ruta de importación según la estructura de tu proyecto
-
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        
-        <Routes>
-          <Route path="/iniciosesion" element={<InicioSesion />} />
-          <Route path="/registro" element={<Registro />} />
-          {/* Agrega más rutas según sea necesario */}
-        </Routes>
-      </div>
-    </Router>
+    <>
+    
+    </>
   );
 }
 
