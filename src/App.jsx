c479158b5638ac45cf2261278416fa1e7745a015
Stiven Dorado <golds9013@gmail.com>
2024-07-02 17:11:53 -0500

    Ultimo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
 import './App.css'
-import Clock from "./components/Clock"
 
 function App() {
   const [count, setCount] = useState(0)
     return (
-      <div>
-        <main>
-          <Clock />
-        </main>
-      </div>
+      <>
+      </>
     );
 }
 
